e59d6029606b7ec4977890d034dc9d71ddd1fecd
ChaimaaFlh <chmfallah@gmail.com>
2026-09-25 14:49:20 +0100

    Update CORS paths in configuration
    Removed 'login' and 'logout' paths from CORS configuration.

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => array_values(array_filter(array_map(
         'trim',
